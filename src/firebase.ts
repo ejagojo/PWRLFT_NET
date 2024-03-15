e694bfabf6a3962daa0ddeb3980f8db094a766e6
ejagojo <agojo.eljohn@gmail.com>
2024-03-14 23:09:19 -0400

    Hid api config

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,13 +8,13 @@ import {getFirestore} from "@firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDePz-aV1vwBaDYUVnAaHfN-IrsyhlJ2bs",
-  authDomain: "pwrlft-net.firebaseapp.com",
-  projectId: "pwrlft-net",
-  storageBucket: "pwrlft-net.appspot.com",
-  messagingSenderId: "947228351193",
-  appId: "1:947228351193:web:eb3d2025ce4cb38b4a3cc8",
-  measurementId: "G-XJCNHLS5BV"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
